3fa1782ae54a5b739b745611c69a2f982ae26af9
darkotosic <darkotosic1986@gmail.com>
2025-06-15 10:09:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["media.api-sports.io"],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: "https://today-api-7f3i.onrender.com",
+  },
+};
 
+module.exports = nextConfig;
